4481aaf3d42ac201ecce142642e368a1372eccc6
Mat <mat555333@gmail.com>
2020-11-30 20:09:16 +0100

    heartbeat on trigger

---
+++ Assets/HeartBeatTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartBeatTrigger : MonoBehaviour
+{
+    Animator animator;
+    private void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        GameObject.FindObjectOfType<GameEvents>().onBeat += HeartBeat;
+    }
+
+    private void HeartBeat()
+    {
+        animator.SetTrigger("HeartBeat");
+    }
+}
