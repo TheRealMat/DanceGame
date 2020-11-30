cd9b133ebe8f172ac8b92bafe430702c13f79076
Mat <mat555333@gmail.com>
2020-11-30 12:53:46 +0100

    init

---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    public LevelMusic levelMusic;
+    public AudioSource musicSource;
+
+    //The number of seconds for each song beat
+    public float secPerBeat;
+
+    //Current song position, in seconds
+    public float songPosition;
+
+    //Current song position, in beats
+    public float songPositionInBeats;
+
+    //How many seconds have passed since the song started
+    public float dspSongTime;
+
+    void Start()
+    {
+
+        //Calculate the number of seconds in each beat
+        secPerBeat = 60f / levelMusic.BPM;
+
+        //Record the time when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        //Start the music
+        musicSource.Play();
+    }
+
+    void Update()
+    {
+        //determine how many seconds since the song started
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - levelMusic.firstBeatOffset);
+
+        //determine how many beats since the song started
+        songPositionInBeats = songPosition / secPerBeat;
+    }
+}
---
+++ Assets/Scripts/LevelMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/LevelMusic", order = 1)]
+public class LevelMusic : ScriptableObject
+{
+    public AudioClip Song;
+
+    public float BPM;
+
+    //The offset to the first beat of the song in seconds
+    public float firstBeatOffset;
+
+    public float lastBeatPosition;
+}
