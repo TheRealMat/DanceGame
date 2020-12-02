af270c6f9e477a7b525c392acec7b45be9f4df0b
Mat <mat555333@gmail.com>
2020-12-02 08:47:08 +0100

    menu logic

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject beatMenu;
+    public GameObject optionsMenu;
+
+    // i'm not sure there's a good way to do this, oh well!
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void CloseOptions()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+
+    public void OpenBeat()
+    {
+        beatMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    public void CloseBeat()
+    {
+        beatMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+
+    public void OpenMenu()
+    {
+        mainMenu.SetActive(true);
+    }
+    public void CloseMenu()
+    {
+        mainMenu.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/SubMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SubMenu : MonoBehaviour
+{
+    public Button defaultSelection;
+
+    private void OnEnable()
+    {
+        Debug.Log("AAAARG");
+        setDefaultSelection();
+    }
+
+    public void setDefaultSelection()
+    {
+        // select button
+        EventSystem.current.SetSelectedGameObject(defaultSelection.gameObject);
+
+        // highlight button
+        defaultSelection.OnSelect(null);
+    }
+}
