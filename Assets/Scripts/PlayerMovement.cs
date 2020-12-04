886df838e07e87a6a1c1c0b64174e65b9d9a1e26
Mat <mat555333@gmail.com>
2020-12-04 08:21:05 +0100

    basic player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    GameManager gameManager;
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        gameManager.events.onPlayerMove += PlayerMove;
+    }
+    private void OnDisable()
+    {
+        gameManager.events.onPlayerMove -= PlayerMove;
+    }
+
+    void PlayerMove(int x, int y)
+    {
+        // do this with an animation or something?
+        transform.position = new Vector2(transform.position.x + x, transform.position.y + y);
+    }
+}
