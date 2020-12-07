d742906969d8fe84c41bd9800f0231179f451f7b
Mat <mat555333@gmail.com>
2020-12-07 13:41:57 +0100

    simple level loading

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class GameManager : MonoBehaviour
     public Settings settings;
     private bool paused = false;
 
+    string[] levels = new string[] { "MainMenu", "Level1" };
+    int currentScene = 0;
 
     // should be relative to BPM somehow. i could maybe take the bpm from the song and then calculate like 5 bpm in either direction from player offset
     private float marginOfError;
@@ -28,5 +31,24 @@ public class GameManager : MonoBehaviour
             conductor.Continue();
         }
         
+    }
+    public void StartNextScene()
+    {
+        // !!! check if this gives build errors !!!
+        SceneManager.LoadScene(levels[currentScene + 1]);
+    }
+
+
+    void Awake()
+    {
+        GameManager[] objs = GameObject.FindObjectsOfType<GameManager>();
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
     }
 }
