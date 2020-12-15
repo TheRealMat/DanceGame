5931217c798b201fbcb04e6a306739d5e222913b
Mat <mat555333@gmail.com>
2020-12-15 13:36:32 +0100

    check if hits enemy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -20,13 +21,29 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         // really needs something to prevent it from moving while it's already moving
+        // also should probably snap to whole numbers
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, desiredPosition, step);
     }
 
     void PlayerMove(int x, int y)
-    {
-        // do this with an animation or something?
-        desiredPosition = new Vector2(transform.position.x + x, transform.position.y + y);
+    {   
+
+
+        // check if moving into an enemy
+        EnemyScript enemy = (from item in gameManager.gameEntities
+                    where item.transform.position.x == transform.position.x + x && item.transform.position.y == transform.position.y + y
+                             select item).FirstOrDefault();
+
+        if (enemy == null)
+        {
+            // do this with an animation or something?
+            desiredPosition = new Vector2(transform.position.x + x, transform.position.y + y);
+        }
+        else
+        {
+            // attack enemy
+        }
+
     }
 }
