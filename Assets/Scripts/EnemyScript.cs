12bbc58f6c34e1c3b8fbf5eafbe06d5dd6aad4d8
Mat <mat555333@gmail.com>
2020-12-16 09:23:01 +0100

    visual attacks

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -11,6 +11,8 @@ public class EnemyScript : MonoBehaviour
     Vector3 desiredPosition;
     float speed = 10;
     int damage = 1;
+    public bool attacking = false;
+    Vector3 previousPosition;
 
     private void Start()
     {
@@ -46,6 +48,15 @@ public class EnemyScript : MonoBehaviour
         // also should probably snap to whole numbers
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, desiredPosition, step);
+
+        if (attacking)
+        {
+            if (transform.position == desiredPosition)
+            {
+                desiredPosition = previousPosition;
+                attacking = false;
+            }
+        }
     }
     public void Attack(Damageable damageable)
     {
@@ -59,6 +70,8 @@ public class EnemyScript : MonoBehaviour
         xDistance = this.transform.position.x - player.transform.position.x;
         yDistance = this.transform.position.y - player.transform.position.y;
 
+        previousPosition = this.transform.position;
+
         // if this can't be done in a less stupid way i'll heat my shoe
         if (Mathf.Abs(xDistance) > Mathf.Abs(yDistance))
         {
@@ -70,6 +83,8 @@ public class EnemyScript : MonoBehaviour
                 {
                     // attack player
                     Attack(player.GetComponent<Damageable>());
+                    desiredPosition = transform.position + Vector3.right / 2;
+                    attacking = true;
                 }
                 else
                 {
@@ -82,6 +97,8 @@ public class EnemyScript : MonoBehaviour
                 {
                     // attack player
                     Attack(player.GetComponent<Damageable>());
+                    desiredPosition = transform.position + Vector3.left / 2;
+                    attacking = true;
                 }
                 else
                 {
@@ -98,6 +115,8 @@ public class EnemyScript : MonoBehaviour
                 {
                     // attack player
                     Attack(player.GetComponent<Damageable>());
+                    desiredPosition = transform.position + Vector3.up / 2;
+                    attacking = true;
                 }
                 else
                 {
@@ -110,6 +129,8 @@ public class EnemyScript : MonoBehaviour
                 {
                     // attack player
                     Attack(player.GetComponent<Damageable>());
+                    desiredPosition = transform.position + Vector3.down / 2;
+                    attacking = true;
                 }
                 else
                 {
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     PlayerScript playerScript;
     float speed = 10;
     public Vector3 desiredPosition;
+    public bool attacking = false;
+    Vector3 previousPosition;
+
     void Start()
     {
         playerScript = FindObjectOfType<PlayerScript>();
@@ -26,11 +29,19 @@ public class PlayerMovement : MonoBehaviour
         // also should probably snap to whole numbers
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, desiredPosition, step);
+        if (attacking)
+        {
+            if(transform.position == desiredPosition)
+            {
+                desiredPosition = previousPosition;
+                attacking = false;
+            }
+        }
     }
 
     void PlayerMove(int x, int y)
-    {   
-
+    {
+        previousPosition = this.transform.position;
 
         // check if moving into an enemy
         EnemyScript enemy = (from item in gameManager.gameEntities
@@ -46,6 +57,8 @@ public class PlayerMovement : MonoBehaviour
         {
             // attack enemy
             playerScript.Attack(enemy.GetComponent<Damageable>());
+            desiredPosition = new Vector2(transform.position.x + (float)x / 2, transform.position.y + (float)y / 2);
+            attacking = true;
         }
 
     }
